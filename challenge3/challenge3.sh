a281bb8f54d373bab2be8cbe3a0e90eb0681b2ad
Rajan Kumar <m1082141@mindtree.com>
2023-02-23 13:13:13 +0530

    adding challenge 3

---
+++ challenge3/challenge3.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -euo pipefail
+
+json_data = $(cat <<EOF
+	{
+		"users": [
+			{
+				"first_name": "John",
+				"last_name": "Smith",
+				"City": "Oklahoma",
+				"immigrationStauts": {
+					"allowed": false
+				}
+			},
+			{
+				"first_name": "Kevin",
+				"last_name": "Cordon",
+				"City": "New Jersey",
+				"immigrationStauts": {
+					"allowed": true
+				}
+			},
+			{
+				"first_name": "James",
+				"last_name": "Anderson",
+				"City": "Houston",
+				"immigrationStauts": {
+					"allowed": true
+				}
+			}
+		]
+	}
+)
+
+echo $json_data 2> /dev/null || returnvalue=$?
+
+if [[ returnvalue == 0 ]]; 
+then
+    echo "json data is available"
+		echo "parsing json data ....."
+    
+		for users in $(echo "${json_data}" | jq -c '.users'); do
+
+			FirstName=$(echo "${users}" | jq -r '.first_name')
+			LastName=$(echo "${users}" | jq -r '.last_name')
+			immigrationStauts=$(echo "${users}" | jq -r '.immigrationStauts.allowed')
+
+			echo "firstName : ${FirstName}"
+			echo "lastName : ${LastName}"
+			echo "ImmigrationStatus : ${immigrationStauts}"
+		done
+else
+    echo "no json data available"
+fi
