7542f271e52bfb65c84a19e8821cb87d74f2e713
Rajan Kumar <m1082141@mindtree.com>
2023-02-23 13:50:37 +0530

    adding challenge 3

--- challenge3/challenge3.sh
+++ challenge3/challenge3.sh
@@ -37,15 +37,15 @@ echo $json_data 2> /dev/null || returnvalue=$?
 if [[ returnvalue == 0 ]]; 
 then
   echo "json data is available"    
-		for users in $(echo "${json_data}" | jq -c '.users'); do
+		for users in $(echo "${json_data}" | jq -r '.users[] | @base64' ); do
 
-			FirstName=$(echo "${users}" | jq -r '.first_name')
-			LastName=$(echo "${users}" | jq -r '.last_name')
-			immigrationStauts=$(echo "${users}" | jq -r '.immigrationStauts.allowed')
+			decode_json_data = $(echo $users | base64 --decode)
 
-			echo "firstName : ${FirstName}"
-			echo "lastName : ${LastName}"
-			echo "ImmigrationStatus : ${immigrationStauts}"
+			FirstName=$(echo "${decode_json_data}" | jq -r '.first_name')
+			LastName=$(echo "${decode_json_data}" | jq -r '.last_name')
+			immigrationStauts=$(echo "${decode_json_data}" | jq -r '.immigrationStauts.allowed')
+
+			echo "User is : ${FirstName} ${LastName} and ImmigrationStatus is ${immigrationStauts}"
 		done
 else
     echo "no json data available"
