a89f895f5a8fac8cc8b8023f2957c72135092598
Rajan Kumar <m1082141@mindtree.com>
2023-03-01 08:54:15 +0530

    adding challenge 2

--- challenge2/ec2-metadata.py
+++ challenge2/ec2-metadata.py
@@ -32,13 +32,5 @@ def get_metadata_json():
     return metadata_json
 
 
-def is_json(myjson):
-    try:
-        json.loads(myjson)
-    except ValueError:
-        return False
-    return True
-
-
 if __name__ == '__main__':
     print(get_metadata_json())
